37134c34b62d6dc45af87a400e19c549e76d07d6
Carolina Cruz <caro21_cruz@hotmail.com>
2022-10-10 23:05:23 -0500

    Se agrego contador e inicio de quiz

--- script.js
+++ script.js
@@ -45,3 +45,56 @@ var listQuestions=[
         answer: "console log"
     },
 ];
+//Contador
+function startTimer() {
+    var time;
+        time = setInterval(function () {
+            timeCount--;
+            contador.textContent=timeCount;
+        if(questionOn===listQuestions.length){
+            clearInterval(time);
+            contador.textContent="0";
+            Viewhighscore();  
+        }else{
+            startGame(questionOn);
+        }
+        if(timeCount===0){
+            clearInterval(time);
+            Viewhighscore();
+
+        }
+        },1000);  
+        
+}
+//Inicio del Quiz
+function start(){
+    //Titulo
+    questionsSec.innerHTML ="";
+    var createH1=document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "Coding Quiz Challenge";
+    questionsSec.appendChild(createH1);
+    //Parrafo de inicio 
+    var createp1=document.createElement("p");
+    createp1.setAttribute("id", "createp1");
+    createp1.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answer will penalize your score/time by ten seconds!";
+    createp1.setAttribute("style", "color:black; font-size:1.5em; justify-content:center;margin:20px auto; align-text:center;");
+    questionsSec.appendChild(createp1);
+
+    //Boton para iniciar quiz
+    var startquiz=document.createElement("button");
+    startquiz.setAttribute("id", "startquiz");
+    startquiz.textContent = "Start Quiz";
+    questionsSec.appendChild(startquiz);
+
+    startquiz.addEventListener("click", function(event) {
+        event.preventDefault();
+        startTimer();
+        startGame(questionOn);
+    });
+    //Botton para ver highscore
+        highscore.addEventListener("click", function(event) {
+        event.preventDefault();
+        showLi();
+    });
+}
